fb5fe4bb42d065c6df31bbaf9fdc895283793cda
Phemieny7 <oyewo.oluwafemi@gmail.com>
2021-08-19 04:23:48 -0700

    login

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,58 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+import Server from '../Server'
+
+const providers = [
+  Providers.Credentials({
+    name: 'Credentials',
+    authorize: async (credentials) => {
+      try {
+        const user = await Server.post(`/login`,
+        {
+            password: credentials.password,
+            email: credentials.email
+        },
+        {
+          headers: {
+            accept: '*/*',
+            'Content-Type': 'application/json'
+          }
+        })
+
+        if (user) {
+          return {status: 'success', data: user.data.user}
+        } 
+      } catch (e) {
+        const errorMessage = e.response.data.message
+        // Redirecting to the login page with error messsage in the URL
+        throw new Error(errorMessage + '&email=' + credentials.email)
+      }
+
+    }
+  })
+]
+
+const callbacks = {
+  async jwt(token, user) {
+    if (user) {
+      token.accessToken = user.data.token
+    }
+
+    return token
+  },
+
+  async session(session, token) {
+    session.accessToken = token.accessToken
+    return session
+  }
+}
+
+const options = {
+  providers,
+  callbacks,
+  pages: {
+    error: '/login' // Changing the error redirect page to our custom login page
+  }
+}
+
+export default (req, res) => NextAuth(req, res, options)
