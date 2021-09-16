39f49f35a4612386aa88333d9b85184bc46e33b6
Phemieny7 <oyewo.oluwafemi@gmail.com>
2021-09-16 01:22:54 -0700

    working

--- layouts/Admin.js
+++ layouts/Admin.js
@@ -14,7 +14,6 @@ import routes from "AdminRoutes.js";
 import styles from "assets/jss/nextjs-material-dashboard/layouts/adminStyle.js";
 import bgImage from "assets/img/sidebar-2.jpg";
 import logo from "assets/img/reactlogo.png";
-
 let ps;
 
 export default function Admin({ children, ...rest }) {
