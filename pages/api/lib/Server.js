26d32524fab3d66e91239a1e72a8461115299cac
Phemieny7 <oyewo.oluwafemi@gmail.com>
2021-09-16 16:37:18 -0700

    add bode

--- pages/api/lib/Server.js
+++ pages/api/lib/Server.js
@@ -1,8 +1,4 @@
 import axios from 'axios';
-import createAuthRefreshInterceptor from 'axios-auth-refresh'
-import * as cookie from 'cookie'
-import * as setCookie from 'set-cookie-parser'
-
 const Server = axios.create({
     baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
     withCredentials: true 
