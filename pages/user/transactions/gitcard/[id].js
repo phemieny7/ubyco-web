158ef8eb67bb91c65121c95d2a71d1e7d09e6a4b
phemieny7 <oyewo.oluwafemi@gmail.com>
2021-12-02 13:22:56 +0100

    works

---
+++ pages/user/transactions/gitcard/[id].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Id() {
+    return (
+        <div>
+            <h1>Life is good</h1>
+        </div>
+    )
+}
+
+export default Id
---
+++ pages/user/transactions/gitcard/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <div>
+            <h1>Life is better</h1>
+        </div>
+    )
+}
